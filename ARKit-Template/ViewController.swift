2156df7901fe353bfe3b31c71b83bb69ab3084d9
Dana Runge <7285068+mydana@users.noreply.github.com>
2025-05-09 10:45:57 -0700

    Update ViewController.swift
    Exercise

--- ARKit-Template/ViewController.swift
+++ ARKit-Template/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
+        
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
