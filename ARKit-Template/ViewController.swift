5f1f6d10af379a77f8b6cc10296792697e80efce
Dana Runge <7285068+mydana@users.noreply.github.com>
2025-05-09 15:16:44 -0700

    Finding Flat Surfaces
    and some jet fighters while we're at it.

--- ARKit-Template/ViewController.swift
+++ ARKit-Template/ViewController.swift
@@ -22,13 +22,59 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
+        print("did load")
+        
         // Set the scene to the view
         sceneView.scene = scene
+    }
+
+    func createFloor(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let node = SCNNode()
+        
+        let geometry = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        node.geometry = geometry
+        print("------------")
+        print(CGFloat(planeAnchor.extent.x), CGFloat(planeAnchor.extent.z))
+
+        node.eulerAngles.x = -Float.pi / 2
+        node.opacity = 0.25
+        
+        return node
+    }
+
+    func createShip(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let node = SCNScene(named: "art.scnassets/ship.scn")!.rootNode.clone()
+        node.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+
+        return node
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+    
+        let floor = createFloor(planeAnchor: planeAnchor)
+        node.addChildNode(floor)
+        
+        let ship = createShip(planeAnchor: planeAnchor)
+        node.addChildNode(ship)
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor
+            else { return }
+        
+        for node in node.childNodes {
+            node.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+            if let plane = node.geometry as? SCNPlane {
+                plane.width = CGFloat(planeAnchor.extent.x)
+                plane.height = CGFloat(planeAnchor.extent.z)
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,6 +82,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+
+        // Enable horizontal plan detection
+        configuration.planeDetection = [.horizontal]
 
         // Run the view's session
         sceneView.session.run(configuration)
